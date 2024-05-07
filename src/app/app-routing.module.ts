18b053349735bcd04432c8428806948865a6ff32
adiez <adiez@riu.com>
2024-05-07 15:30:12 -0300

    feat: routing settings

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListaSuperheroesComponent } from './pages/lista-superheroes/lista-superheroes.component';
+import { DetalleSuperheroeComponent } from './pages/detalle-superheroe/detalle-superheroe.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/heroes', pathMatch: 'full' },
+  { path: 'heroes', component: ListaSuperheroesComponent },
+  { path: 'heroes/:id', component: DetalleSuperheroeComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
