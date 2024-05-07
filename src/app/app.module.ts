b51d2a1ad063b1e672ab35175ec813099a5ef8cd
adiez <adiez@riu.com>
2024-05-07 15:34:54 -0300

    feat: update app.modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,49 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import { ListaSuperheroesComponent } from './pages/lista-superheroes/lista-superheroes.component';
+import { DetalleSuperheroeComponent } from './pages/detalle-superheroe/detalle-superheroe.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatCardModule } from '@angular/material/card';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { ModalAgregarHeroeComponent } from './pages/modal-agregar-heroe/modal-agregar-heroe.component';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
+import { ConfirmarBorradoComponent } from './components/confirmar-borrado/confirmar-borrado.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListaSuperheroesComponent,
+    DetalleSuperheroeComponent,
+    ModalAgregarHeroeComponent,
+    ConfirmarBorradoComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FormsModule,
+    MatToolbarModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    MatListModule,
+    MatCardModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatDialogModule,
+    MatButtonModule,
+    MatDividerModule,
+    ReactiveFormsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
