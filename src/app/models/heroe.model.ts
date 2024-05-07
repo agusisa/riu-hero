81864c2bfc2e71d52ec1e3fd21daed8f9f0aedd5
adiez <adiez@riu.com>
2024-05-07 15:32:05 -0300

    feat: heroe model implementation

---
+++ src/app/models/heroe.model.ts
@@ -0,0 +1,12 @@
+// models/heroe.model.ts
+export interface Heroe {
+  id: number;
+  nombre: string;
+  poder: string;
+  edad: number;
+  planetaOrigen: string;
+  archienemigo: string;
+  estatura: number;
+  puntoDebil: string;
+  origenPoderes: string;
+}
